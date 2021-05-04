3875bc413ddce491213c2e00858255facbce0320
Cipher <asaforbrian18@gmail.com>
2021-05-04 22:11:33 +0100

    Skeleton of the app
    The code here portrays the basic structure or the blueprint for a Tkinter Gui application.

---
+++ main.py
@@ -0,0 +1,21 @@
+# Building a Registration Gui application with Tkinter
+# Step 1: import Tkinter and all its required Modules
+
+from tkinter import *
+
+# Create a main window (object): Let's call it window
+
+window = Tk()
+
+# Providing Geometry to the Application
+
+window.geometry("500x500")
+
+# Changing the background color of our window
+window.config(bg="cyan")
+
+
+
+# This will run the mainloop
+
+window.mainloop()
